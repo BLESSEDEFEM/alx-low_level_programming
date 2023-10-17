87249ec36cdb3ee9834acb5a6a93c6371720de25
Blessing <nejo.blessing@yahoo.com>
2023-10-17 13:52:16 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/arithmetic.c
@@ -0,0 +1,29 @@
+int sum(int a,  int b)
+{
+	int addition = (a + b);
+	return (addition);
+}
+
+int sub(int a, int b)
+{
+	int subtraction = (a - b);
+	return (subtraction);
+}
+
+int mul(int a, int b)
+{
+	int product = (a * b);
+	return (product);
+}
+
+int div(int a, int b)
+{
+	int division = (a / b);
+	return (division);
+}
+
+int mod(int a, int b)
+{
+	int modulus = (a % b);
+	return (modulus);
+}
