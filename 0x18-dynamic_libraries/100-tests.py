cbc8113a2b045b2578376824f46aaa710f0813b6
Blessing <nejo.blessing@yahoo.com>
2023-10-17 14:48:04 -0700

    dynamic libraries

--- 0x18-dynamic_libraries/100-tests.py
+++ 0x18-dynamic_libraries/100-tests.py
@@ -1,15 +1,15 @@
 import ctypes
 
 a = 66
-b = 76
-cops = ctypes.CDLL('./100-operations.so')
+b = -76
+arithmetic = ctypes.CDLL('./100-operations.so')
 
-print(f"{a} + {b} = {cops.sum(a, b)}")
+print(f"{a} + {b} = {arithmetic.sum(a, b)}")
 
-print(f"{a} - {b} = {cops.sub(a, b)}")
+print(f"{a} - {b} = {arithmetic.sub(a, b)}")
 
-print(f"{a} * {b} = {cops.mul(a, b)}")
+print(f"{a} * {b} = {arithmetic.mul(a, b)}")
 
-print(f"{a} / {b} = {cops.div(a, b)}")
+print(f"{a} / {b} = {arithmetic.div(a, b)}")
 
-print(f"{a} % {b} = {cops.mod(a, b)}")
+print(f"{a} % {b} = {arithmetic.mod(a, b)}")
