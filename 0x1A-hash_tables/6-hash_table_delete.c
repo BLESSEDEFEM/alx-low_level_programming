38ae2eca82598704eb50701972d5abf6b7cfc16a
Blessing <nejo.blessing@yahoo.com>
2023-11-05 15:17:39 -0800

    hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -17,7 +17,7 @@ void hash_table_delete(hash_table_t *ht)
 			node = ht->array[i];
 			while (node != NULL)
 			{
-				tmp = node->nexy;
+				tmp = node->next;
 				free(node->key);
 				free(node->value);
 				free(node);
